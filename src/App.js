209c3e4101adfec920ba7f70b63f5953a03a96b2
Sam Hollyer <rsamhollyer@gmail.com>
2021-01-21 14:47:50 -0600

    updated for props

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import { useEffect } from "react";
 import GetJoke from "./Getjoke";
 
 function App() {
-	// tells React to perform actions that are not directly related to drawing -- side-effects
-
 	useEffect(() => {
+		// tells React to perform actions that are not directly related to drawing -- side-effects
+		//This side effect will run exactly one time no matter how often App() function is refun
+		// If anything needs redrawing in the live DOM on the page.
+		// This calculation and reconciliation process is known as React's Virtual DOM
 		GetJoke();
 	});
 
@@ -16,7 +18,7 @@ function App() {
 		<div className="App">
 			<Header />
 			<JokeButton />
-			<JokeContainer />
+			<JokeContainer joke="Knock Knock" />
 		</div>
 	);
 }
--- src/JokeContainer.js
+++ src/JokeContainer.js
@@ -1,9 +1,10 @@
 import React from "react";
 
-function JokeContainer() {
+function JokeContainer(props) {
 	return (
 		<section className="joke-container">
 			<h3>Jokes go Here!</h3>
+			<p>{props.joke}</p>
 		</section>
 	);
 }
